b031283a23798200ae08d716546e891e40493329
zippy565 <156541508+zippy565@users.noreply.github.com>
2024-02-11 21:26:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,6 +25,10 @@ const App = () => {
 
                         {
                             /* Add Remaining component here*/
+                                             //Remaining component
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
                         }        
 
                         {
